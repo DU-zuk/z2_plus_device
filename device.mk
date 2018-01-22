53d53d82824e2d3586a4e954e7bf049013c44977
ZyxxeZ <bankersenator@gmail.com>
2018-01-22 18:16:02 +0000

    du bringout

--- device.mk
+++ device.mk
@@ -40,3 +40,11 @@ PRODUCT_PACKAGES += \
 
 # Inherit from msm8996-common
 $(call inherit-product, device/zuk/msm8996-common/msm8996.mk)
+
+#  Dalvik heap
+ dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
